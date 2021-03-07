ad1043399282b1183cd9394f0e173667d653bb24
nlammers371 <nlammers@berkeley.edu>
2021-03-06 18:49:03 -0800

    Old commits

--- src/utilities/nsMCMC/calculate_fluo_logL_ens_v2.m
+++ src/utilities/nsMCMC/calculate_fluo_logL_ens_v2.m
@@ -1,4 +1,4 @@
-function logL_fluo = calculate_fluo_logL_ens(mcmcInfo)
+function logL_fluo = calculate_fluo_logL_ens_v2(mcmcInfo)
                   
     % extrac useful parameters
     seq_length_dummy = size(mcmcInfo.sample_chains_dummy,1);%mcmcInfo.seq_length;
@@ -18,7 +18,7 @@ function logL_fluo = calculate_fluo_logL_ens(mcmcInfo)
     linIndexArrayFull = indexArrayFull + mcmcInfo.chain_id_ref*seq_length_dummy + mcmcInfo.trace_id_ref*seq_length_dummy*n_chains;  
     
     % extract states
-    initiation_fragment_init = mcmcInfo.v(mcmcInfo.sample_chains_dummy(linIndexArrayFull));
+    initiation_fragment_init = mcmcInfo.v_curr(mcmcInfo.sample_chains_dummy(linIndexArrayFull));
     
     % zero out dummy placeholder states
     dummyFilter = indexArrayFull-nSteps+1<1 | indexArrayFull-nSteps+1>seq_length;
@@ -26,7 +26,7 @@ function logL_fluo = calculate_fluo_logL_ens(mcmcInfo)
     
     % add proposed states    
     initiation_fragment = repmat(initiation_fragment_init,1,1,1,nStates);
-    initiation_fragment(nSteps,:,:,:) = repmat(reshape(mcmcInfo.v,1,1,1,[]),1,n_chains,n_traces);%repmat(mcmcInfo.sample_chains_slice(prevInd:postInd,:),1,1,nStates);           
+    initiation_fragment(nSteps,:,:,:) = repmat(reshape(mcmcInfo.v_curr,1,1,1,[]),1,n_chains,n_traces);%repmat(mcmcInfo.sample_chains_slice(prevInd:postInd,:),1,1,nStates);           
                                         
     % calculate predicted fluorescence
     fluo_fragment = convn(coeff_MS2,initiation_fragment,'full');             
@@ -37,6 +37,7 @@ function logL_fluo = calculate_fluo_logL_ens(mcmcInfo)
     ref_fluo = repmat(mcmcInfo.observed_fluo_dummy(linIndexArrayFluo),1,1,1,nStates);
     dummy_trunc = dummyFilter(nSteps:end,:,:);
     ref_fluo(dummy_trunc) = fluo_fragment(dummy_trunc);
+    
     % set leading and trailing observations equal to model fluo. This is
     % the easiest way to remove from likelihood calculation
     logL_fluo_full = -0.5*(((ref_fluo-fluo_fragment)./sigma_curr).^2 + log(2*pi*sigma_curr.^2));
