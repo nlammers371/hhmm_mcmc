ca7260bfc6018a8562b7f3bc625366ba106d5221
nlammers371 <nlammers@berkeley.edu>
2021-03-06 19:33:51 -0800

    Work in progress to merge asynchronous updating with parallel chain inference

---
+++ src/utilities/nsMCMC/resample_chains_v3.m
@@ -0,0 +1,94 @@
+function mcmcInfo = resample_chains_v3(mcmcInfo)
+
+% extract parameters
+A_log = log(mcmcInfo.A_curr);
+pi0 = mcmcInfo.pi0_curr;
+pi0_log = log(pi0);
+nStates = mcmcInfo.nStates;
+n_traces = mcmcInfo.n_traces;
+n_chains = mcmcInfo.n_chains;
+nSteps = mcmcInfo.nSteps;
+seq_length = mcmcInfo.seq_length;
+
+% generate reference vector
+mcmcInfo.chain_id_ref = 0:n_chains-1;
+mcmcInfo.trace_id_ref = reshape(0:n_traces-1,1,1,[]);
+mcmcInfo.row_ref = (1:nStates)';
+mcmcInfo.step_ref = (-nSteps+1:nSteps-1)';
+
+% generate random sampling orders
+n_reps = 1;
+sample_indices = NaN(n_reps*seq_length,size(mcmcInfo.sample_chains,2),size(mcmcInfo.sample_chains,3));
+for n = 1:n_chains
+    for t = 1:n_traces
+        sample_indices(:,n,t) = randsample(repelem(1:seq_length,n_reps),n_reps*seq_length,false);
+    end
+end
+
+% generate temporary chain array
+sample_chains_temp = NaN(size(mcmcInfo.sample_chains,1)+2,size(mcmcInfo.sample_chains,2),size(mcmcInfo.sample_chains,3));
+sample_chains_temp(2:end-1,:,:) = mcmcInfo.sample_chains;
+
+% initialize starting and end indices randomly using pi0
+sample_chains_temp(1,:,:) = reshape(randsample(1:nStates,n_chains*n_traces,true,pi0),1,n_chains,n_traces);
+sample_chains_temp(end,:,:) = reshape(randsample(1:nStates,n_chains*n_traces,true,pi0),1,n_chains,n_traces);
+seq_len_temp = seq_length+2;
+seq_len_dummy = seq_length+2*(nSteps-1);
+
+% create dummy chain array for fluorescence sampling
+% true_states = repmat(permute(vertcat(mcmcInfo.masterSimStruct.naive_states)',[1 3 2]),1, n_chains,1);
+
+% true_states =repmat(reshape(vertcat(mcmcInfo.masterSimStruct.naive_states)',seq_length,1,n_traces),1,n_chains,1);
+% warning('using actua; states for testing')
+mcmcInfo.sample_chains_dummy = cat(1,ones(nSteps-1,n_chains,n_traces),mcmcInfo.sample_chains,ones(nSteps-1,n_chains,n_traces));
+mcmcInfo.observed_fluo_dummy = cat(1,zeros(nSteps-1,n_traces),mcmcInfo.observed_fluo,zeros(nSteps-1,n_traces));
+
+% iterate through indices to sample
+for i = 1:seq_length
+    mcmcInfo.indArray = sample_indices(i,:,:);    
+    
+    prev_time_index_array = sample_indices(i,:,:);    
+    prev_lin_index_array = prev_time_index_array + mcmcInfo.chain_id_ref*seq_len_temp + mcmcInfo.trace_id_ref*seq_len_temp*n_chains;
+    post_lin_index_array = prev_lin_index_array + 2;
+    
+    %%% previous state %%%
+    % calculate linear indices 
+    prev_state_array = sample_chains_temp(prev_lin_index_array);
+    row_col_array_from = (prev_state_array-1)*nStates+mcmcInfo.row_ref;    
+    
+    % extract probabilities
+    prev_probs_log = A_log(row_col_array_from);
+          
+    %%% following state %%%
+    % calculate linear indices 
+    post_state_array = sample_chains_temp(post_lin_index_array);  
+    row_col_array_to = (mcmcInfo.row_ref-1)*nStates+post_state_array;
+        
+    % extract probabilities
+    post_probs_log = A_log(row_col_array_to);
+       
+    % combine
+    logL_tr = prev_probs_log + post_probs_log;
+
+    %%% calculate fluorescence probability component
+
+    % calculate fluo error term          
+    logL_fluo = calculate_fluo_logL_ens(mcmcInfo);
+    
+    %%% put everything together
+    total_log_likelihoods = logL_tr + logL_fluo;
+    total_log_likelihoods = total_log_likelihoods - logsumexp(total_log_likelihoods,1);
+    total_likelihoods = exp(total_log_likelihoods);    
+
+    %%% draw new samples
+    option_array = cumsum(total_likelihoods);
+    rand_array = repmat(rand(1,n_chains,n_traces),nStates,1);
+%     
+%     mcmcInfo.sample_chains(curr_lin_index_array) = sum(rand_array > option_array) + 1;
+    sample_chains_temp(prev_lin_index_array+1) = sum(rand_array > option_array) + 1;
+    curr_lin_index_array = (mcmcInfo.indArray+nSteps-1) + mcmcInfo.chain_id_ref*seq_len_dummy + mcmcInfo.trace_id_ref*seq_len_dummy*n_chains;
+    mcmcInfo.sample_chains_dummy(curr_lin_index_array) = sample_chains_temp(prev_lin_index_array+1);
+
+end  
+
+mcmcInfo.sample_chains = sample_chains_temp(2:end-1,:,:);
---
+++ src/utilities/nsMCMC/update_hmm_parameters_v3.m
@@ -0,0 +1,81 @@
+function mcmcInfo = update_hmm_parameters_v3(mcmcInfo)    
+
+    % extrace parameters
+    nStates = mcmcInfo.nStates;
+    seq_length = mcmcInfo.seq_length;
+    n_traces = mcmcInfo.n_traces;
+    n_chains = mcmcInfo.n_chains;
+    coeff_MS2 = mcmcInfo.coeff_MS2;        
+    
+    %% %%%%%%%%%% update transition matrix (A) %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    update_flag = mod(mcmcInfo.step,mcmcInfo.update_increment) == 0;
+    update_index = mcmcInfo.step/mcmcInfo.update_increment + 1;
+    A_counts = mcmcInfo.transition_count_array;  
+    for n = 1:n_chains
+        mcmcInfo.A_curr(:,:,n) = sample_A_dirichlet(mcmcInfo.A_alpha, A_counts(:,:,n));    
+        
+        % update pi0    
+        [V, D] = eig(mcmcInfo.A_curr(:,:,n));
+        [~, mi] = max(real(diag(D)));
+        mcmcInfo.pi0_curr(n,:) = V(:,mi)/sum(V(:,mi));    
+        
+        if update_flag
+            mcmcInfo.A_inf_array(:,:,update_index,n) = mcmcInfo.A_curr(:,:,n);
+            mcmcInfo.pi0_inf_array(:,:,update_index,n) = mcmcInfo.pi0_curr(n,:);
+        end
+    end
+    
+    
+    
+    
+    
+    %% %%%%%%%%%%%%% update emission vector(V) %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % update V   
+    
+    % generate F count arrays
+    F_array = zeros(seq_length*n_traces*n_chains,nStates);    
+    y_array = NaN(seq_length*n_traces*n_chains,1);    
+    
+    for n = 1:n_traces        
+        ind1 = (n-1)*seq_length*n_chains+1;
+        ind2 = n*seq_length*n_chains;
+        % record observed fluo
+        y_array(ind1:ind2,:) = repmat(mcmcInfo.observed_fluo(:,n),n_chains,1);
+        for m = 1:nStates
+            % record counts
+            state_counts = convn(coeff_MS2,mcmcInfo.sample_chains(:,:,n)==m,'full');            
+            F_array(ind1:ind2,m) = reshape(state_counts(1:end-length(coeff_MS2)+1,:),[],1);                        
+        end
+    end
+       
+    % update for each chain       
+    M = (F_array'*F_array + 1e-4) / n_chains;    
+    b = (F_array'*y_array) / n_chains;
+
+    % calculate mean and variance
+    v_mean = M\b;
+    v_cov_mat = mcmcInfo.sigma_curr^2 * inv(M);
+
+    % sample
+    mcmcInfo.v_curr = mvnrnd(v_mean, v_cov_mat)';             
+    
+    if update_flag
+        mcmcInfo.v_inf_array(update_index,:) = mcmcInfo.v_curr;   
+    end
+    %% %%%%%%%%%%%%% update noise parameter (sigma) %%%%%%%%%%%%%%%%%%%%%%%
+
+    % get predicted fluorescence
+    mcmcInfo = predict_fluo_full_ens(mcmcInfo);
+       
+    % Update sigma
+    a = numel(mcmcInfo.observed_fluo)/2;
+    F_diff = reshape(permute(mcmcInfo.sample_fluo,[1 3 2]) - mcmcInfo.observed_fluo,[],1);
+    b = F_diff'*F_diff / 2 / n_chains;
+
+    % fraw sample
+    mcmcInfo.sigma_curr = sqrt(1./gamrnd(a,1./b));%mcmcInfo.sigma;%sqrt(mean(F_diff.^2));%
+  
+    if update_flag
+        mcmcInfo.sigma_inf_array(update_index) = mcmcInfo.sigma_curr;
+    end
+        
